9ce9c0dcb55fbac51196530c218ce3ff7a5de1c8
Khlopiachyi Pavlo <khlpavlo@gmail.com>
2019-08-20 18:19:13 +0300

    ex09 done

---
+++ ex09-tcp-fibonacci/client/client.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Res struct {
+	Num  *big.Int
+	Time time.Duration
+}
+
+var port = "127.0.0.1:6666"
+
+func fileEncode(conn net.Conn, i int64) {
+
+	enc := json.NewEncoder(conn)
+	err := enc.Encode(i)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+}
+
+func fileDecode(conn net.Conn) {
+	var msg Res
+	dec := json.NewDecoder(conn)
+	err := dec.Decode(&msg)
+	if err != nil {
+		fmt.Println("Decode error: ", err)
+		return
+	}
+	fmt.Printf("%s %d\n", msg.Time, msg.Num)
+}
+
+func main() {
+	conn, err := net.Dial("tcp", port)
+	if err != nil {
+		fmt.Println("Err: ", err)
+		return
+	}
+
+	defer conn.Close()
+
+	scan := bufio.NewScanner(os.Stdin)
+	for scan.Scan() {
+		i, err := strconv.ParseInt(scan.Text(), 10, 64)
+		if err != nil {
+			return
+		}
+		fileEncode(conn, i)
+		fileDecode(conn)
+	}
+}
